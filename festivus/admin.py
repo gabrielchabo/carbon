cd7f872b08140336f2ffe0d34fe037a23723bf1c
Gabriel Chabo <gabrielchabo@gmail.com>
2017-05-30 22:20:41 +0300

    Added Event Management

--- festivus/admin.py
+++ festivus/admin.py
@@ -1,9 +1,36 @@
 from django.contrib import admin
 
-from .models import Person
+from .models import Person, Payment, PlaceCategory, Place, EventType, Event
 
-class FestivusAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'birth_date')
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'birth_date', 'email')
     list_filter = ('gender', 'marital_status')
+    search_fields = ('first_name', 'last_name')
 
-admin.site.register(Person, FestivusAdmin)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('person', 'month', 'amount', 'currency', 'enrolled')
+    list_filter = ('year', 'month', 'currency', 'enrolled')
+    search_fields = ('person__first_name', 'amount')
+
+class PlacesCategoryAdmin(admin.ModelAdmin):
+    search_fields = ('name', '')
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('category', 'name', 'rating')
+    list_filter = ('category', 'rating')
+    search_fields = ('name', 'description', 'comment')
+
+class EventTypeAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'description')
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('event_type', 'victim', 'event_date', 'gift_card')
+    list_filter = ('event_type', 'organizer')
+    search_fields = ('event_type__name', 'description', 'comment', 'location')
+
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Payment, PaymentAdmin)
+admin.site.register(PlaceCategory, PlacesCategoryAdmin)
+admin.site.register(Place, PlaceAdmin)
+admin.site.register(EventType, EventTypeAdmin)
+admin.site.register(Event, EventAdmin)
--- festivus/models.py
+++ festivus/models.py
@@ -1,4 +1,53 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+import datetime
+
+USD = 'USD'
+LBP = 'LBP'
+CURRENCY_CHOICES = (
+    (USD, '$'),
+    (LBP, 'LL'),
+)
+
+JANUARY = 'JAN'
+FEBRUARY = 'FEB'
+MARCH = 'MAR'
+APRIL = 'APR'
+MAY = 'MAY'
+JUNE = 'JUN'
+JULY = 'JUL'
+AUGUST = 'AUG'
+SEPTEMBER = 'SEP'
+OCTOBER = 'OCT'
+NOVEMBER = 'NOV'
+DECEMBER = 'DEC'
+
+MONTH_CHOICES = (
+    (JANUARY, 'January'),
+    (FEBRUARY, 'February'),
+    (MARCH, 'March'),
+    (APRIL, 'April'),
+    (MAY, 'May'),
+    (JUNE, 'June'),
+    (JULY, 'July'),
+    (AUGUST, 'August'),
+    (SEPTEMBER, 'September'),
+    (OCTOBER, 'October'),
+    (NOVEMBER, 'November'),
+    (DECEMBER, 'December'),
+)
+
+YEAR_CHOICES = []
+for r in range(2016, (datetime.datetime.now().year+1)):
+    YEAR_CHOICES.append((r, r))
+
+def validatePhone(value):
+    if not (value.isdigit() and len(value) == 8):
+        raise ValidationError(
+            _('%(value)s is not an valid phone number'),
+            params={'value': value},
+        )
 
 class Person(models.Model):
     MALE = "M"
@@ -23,9 +72,64 @@ class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     birth_date = models.DateField()
+    email = models.EmailField()
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES, default=MALE,)
     marital_status = models.CharField(max_length=10, choices=MARITAL_CHOICES, default=SINGLE,)
     #gender = models.ForeignKey(Gender)
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name.upper()
+
+class Payment(models.Model):
+    person = models.ForeignKey(Person)
+    year = models.IntegerField(('year'), choices=YEAR_CHOICES, default=datetime.datetime.now().year)
+    month = models.CharField(max_length=20, choices=MONTH_CHOICES, default=JANUARY,)
+    amount = models.DecimalField(default=10, max_digits=6, decimal_places=2)
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default=USD,)
+    enrolled = models.BooleanField(default=True)
+
+class PlaceCategory(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Place(models.Model):
+    RATINGS_CHOICES = (
+        (1, 'Unless Necessary'),
+        (2, 'Good Enough'),
+        (3, 'Average'),
+        (4, 'Great'),
+        (5, 'Amazing'),
+  )
+    category = models.ForeignKey(PlaceCategory)
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=100, null=True)
+    phone = models.CharField(max_length=8, validators=[validatePhone], null=True)
+    description = models.CharField(max_length=200, null=True)
+    rating = models.IntegerField(default=3, choices=RATINGS_CHOICES)
+    comment = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+class EventType(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Event(models.Model):
+    event_type = models.ForeignKey(EventType)
+    organizer = models.ForeignKey(Person, related_name='event_organizer')
+    victim = models.ForeignKey(Person)
+    event_date = models.DateField()
+    location = models.CharField(max_length=30, null=True)
+    budget = models.IntegerField(default=0)
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default=USD,)
+    gift_card = models.BooleanField(default=False)
+    comment = models.CharField(max_length=200, null=True)
+
+
+#Relationship
